44eafc8cedbdaea5702493834d25776f303c5c12
Ahmedmalik <159722798+ahmed22138@users.noreply.github.com>
2024-10-02 04:36:50 +0500

    Ahmed zahid
    I am Create a Portfolio website

---
+++ src/app/about/page.tsx
@@ -0,0 +1,24 @@
+
+
+export default function About(){
+    return(
+        <div>
+            <section className="text-gray-600 body-font">
+  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+      <img className="object-cover object-center rounded" alt="hero" src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+    </div>
+    <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center text-white">
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white"/>About
+        <br className="hidden lg:inline-block"/>readymade us
+      
+      <p className="mb-8 leading-relaxed text-white">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+      <div className="flex justify-center">
+    
+      </div>
+    </div>
+  </div>
+</section>
+        </div>
+    )
+}
